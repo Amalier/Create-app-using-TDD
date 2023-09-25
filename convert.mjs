2a4675bda3e98a2cebcf8ecd2f5b18947d1abe70
Amalier <amalierosseland@hotmail.com>
2023-09-25 22:16:16 +0200

    la til input arguments og lagde egen test fil

---
+++ convert.mjs
@@ -0,0 +1,42 @@
+
+
+const inches = 1
+const millimeters = convertInchesToMillimeter(inches);
+const centimeters = convertInchesToCentimeters(inches)
+const meters = convertInchesToMeters(inches)
+
+let args = process.argv ;
+//console.log(args)
+
+const isMillimeter = args.some((item) => item === "-mm") 
+const isCentmeter = args.some((item) => item === "-cm") 
+const isMeter = args.some((item) => item === "-m") 
+const number = parseFloat(args[2])
+ //args.find((item ) => typeof (item * 1) === "number")
+//console.log(number)
+
+if (isMillimeter){
+  console.log(convertInchesToMillimeter(number))
+} else if (isCentmeter){
+  console.log(convertInchesToCentimeters(number))
+} else if (isMeter){
+  console.log(convertInchesToMeters(number))
+}
+
+
+export function convertInchesToMeters(inches){
+    return inches * 0.0254
+}
+
+//kanksje konvertere dette til en  ternary operation for og gjøre det lettere og lese
+export function convertInchesToMillimeter(inches){
+    return inches * 25.4// inch mm er 25,4 mm
+     }
+
+
+  export function convertInchesToCentimeters(inches) {
+    return inches * 2.54;
+  }
+
+
+ 

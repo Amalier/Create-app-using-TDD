54709db72a158a451ccb941ceeb01acd0adf58cd
Amalier <amalierosseland@hotmail.com>
2023-09-25 20:40:03 +0200

    Test failed, testen funker

--- convert.js
+++ convert.js
@@ -1,24 +1,59 @@
 
 const inches = 4
 const millimeters = convertInchesToMillimeter(inches);
+const centimeters = convertInchesToCentimeters(inches)
+const meters = convertInchesToMeters(inches)
 
-function convertInchesToMillimeter(inches){
-return inches * 25.4// 1 mm er 25,2 inches
 
+console.log(meters)
+console.log(millimeters)
+console.log(centimeters)
+
+testconvertInchesToMeters();
+testConvertInchesToMillimeter();
+testConvertInchesToCentimeter();
+
+function testconvertInchesToMeters(){
+    if (convertInchesToMeters(1) === 0.0254) {
+        console.log("Test meters Passed");
+      } else {
+        console.error("Test meters Failed");
+      }
+}
+function convertInchesToMeters(inches){
+    return 0
+    //inches * 0.0254
 }
 
-function convertInchesToMillimeterTester() {
 
-    if (convertInchesToMillimeter(1) === 25,4) {
-      console.log("Test Passed");
+function testConvertInchesToCentimeter(){
+    if (convertInchesToCentimeters(1) === 2.54) {
+        console.log("Test cm Passed");
+      } else {
+        console.error("Test cm Failed");
+      }
+}
+
+//kanksje konvertere dette til en  ternary operation for og gjøre det lettere og lese
+function convertInchesToMillimeter(inches){
+    return 0
+    //inches * 25.4// inch mm er 25,4 mm
+    
+    }
+function testConvertInchesToMillimeter() {
+
+    if (convertInchesToMillimeter(1) === 25.4) {
+      console.log("Test mm Passed");
     } else {
-      console.error(" Test Failed");
+      console.error("Test mm Failed");
     }
   }
+  
 
-  function convertToCentimeters(inches) {
-    return inches * 2.54;
+  function convertInchesToCentimeters(inches) {
+    return 0;
+    // inches * 2.54;
   }
 
- convertInchesToMillimeterTester();
- console.log(millimeters)
+
+ 

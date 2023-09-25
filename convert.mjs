8a07ea68be06df6372c7a4e8d60754cd99c23e27
Amalier <amalierosseland@hotmail.com>
2023-09-25 23:56:33 +0200

    is running check og html update

--- convert.mjs
+++ convert.mjs
@@ -1,22 +1,47 @@
 
 
-let args = process.argv;
-//console.log(args)
-
-const isMillimeter = args.some((item) => item === "-mm")
-const isCentmeter = args.some((item) => item === "-cm")
-const isMeter = args.some((item) => item === "-m")
-const number = parseFloat(args[2])
-//args.find((item ) => typeof (item * 1) === "number")
-//console.log(number)
-
-if (isMillimeter) {
-  console.log(convertInchesToMillimeter(number))
-} else if (isCentmeter) {
-  console.log(convertInchesToCentimeters(number))
-} else if (isMeter) {
-  console.log(convertInchesToMeters(number))
+if (!isRunningInTerminal()) {
+  // I nettleseren
+  const bt = document.getElementById("convertButton");
+  const select = documnet.getElementById("selectConversion")
+  bt.onclick = () => {
+  };
 }
+
+function isRunningInTerminal() {
+  try {
+    if (document) {
+      return false;
+    }
+  } catch (error) {
+    return true;
+  }
+
+  return true;
+}
+if (isRunningInTerminal) {
+  TerminalApp()
+}
+
+function TerminalApp() {
+  let args = process.argv;
+
+  const isMillimeter = args.some((item) => item === "-mm")
+  const isCentmeter = args.some((item) => item === "-cm")
+  const isMeter = args.some((item) => item === "-m")
+  const number = parseFloat(args[2])
+  //args.find((item ) => typeof (item * 1) === "number")
+  //console.log(number)
+
+  if (isMillimeter) {
+    console.log(convertInchesToMillimeter(number))
+  } else if (isCentmeter) {
+    console.log(convertInchesToCentimeters(number))
+  } else if (isMeter) {
+    console.log(convertInchesToMeters(number))
+  }
+}
+
 
 export function convertInchesToMeters(inches) {
   return inches * 0.0254

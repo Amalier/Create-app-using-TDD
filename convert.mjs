01bde78ff8cbca81c9c1f51669ac7b4dda8350a2
Amalier <amalierosseland@hotmail.com>
2023-09-26 20:15:26 +0200

    finished code

--- convert.mjs
+++ convert.mjs
@@ -1,57 +1,80 @@
 
+import { testing } from "./tests.mjs";
 
-if (!isRunningInTerminal()) {
-  // I nettleseren
+export const INCHES_TO_METER = 0.0254;
+export const INCHES_TO_CENTIMETER = 25.4;
+export const INCHES_TO_MILLIMETER = 2.54;
+
+
+if (isRunningInBrowser()) {
   const bt = document.getElementById("convertButton");
-  const select = documnet.getElementById("selectConversion")
   bt.onclick = () => {
-  };
+    const result = convertValue()
+
+    const outputTextElement = document.getElementById("outputtext");
+    outputTextElement.innerHTML = `Result: ${result}`;
+
+  }
+} else {
+
+  let args = process.argv;
+  const shouldTest = args.some((item) => item === "-t")
+  if (shouldTest) {
+    testing();
+  } else {
+    const result = convertValue()
+    console.log(`${result}${unit}`)
+  }
 }
 
-function isRunningInTerminal() {
+function convertValue() {
+  let value;
+  let unit;
+
+  if (isRunningInBrowser()) {
+    value = parseFloat(document.getElementById('inputNumber').value);
+    unit = document.getElementById('selectConversion').value;
+
+  } else {
+    let args = process.argv;
+    value = parseFloat(args[2])
+    unit = args[3]
+  }
+  if (unit === "-mm") {
+    return convertInchesToMillimeters(value);
+  } else if (unit === "-cm") {
+    return convertInchesToCentimeters(value);
+  } else if (unit === "-m") {
+    return convertInchesToMeters(value);
+  } else {
+
+    return convertInchesToMillimeters(value);
+
+  }
+
+}
+
+export function isRunningInBrowser() {
   try {
     if (document) {
-      return false;
+      return true;
     }
   } catch (error) {
-    return true;
+    return false;
   }
 
-  return true;
-}
-if (isRunningInTerminal) {
-  TerminalApp()
+  return false;
 }
 
-function TerminalApp() {
-  let args = process.argv;
-
-  const isMillimeter = args.some((item) => item === "-mm")
-  const isCentmeter = args.some((item) => item === "-cm")
-  const isMeter = args.some((item) => item === "-m")
-  const number = parseFloat(args[2])
-  //args.find((item ) => typeof (item * 1) === "number")
-  //console.log(number)
-
-  if (isMillimeter) {
-    console.log(convertInchesToMillimeter(number))
-  } else if (isCentmeter) {
-    console.log(convertInchesToCentimeters(number))
-  } else if (isMeter) {
-    console.log(convertInchesToMeters(number))
-  }
-}
-
-
-export function convertInchesToMeters(inches) {
-  return inches * 0.0254
-}
 
 //kanksje konvertere dette til en  ternary operation for og gjøre det lettere og lese
-export function convertInchesToMillimeter(inches) {
-  return inches * 25.4// inch mm er 25,4 mm
+export function convertInchesToMillimeters(inches) {
+  return inches * INCHES_TO_MILLIMETER
 }
 
 export function convertInchesToCentimeters(inches) {
-  return inches * 2.54;
+  return inches * INCHES_TO_CENTIMETER
+}
+export function convertInchesToMeters(inches) {
+  return inches * INCHES_TO_METER
 }
--- tests.mjs
+++ tests.mjs
@@ -1,40 +1,51 @@
 
-import { convertInchesToMeters, convertInchesToCentimeters, convertInchesToMillimeter } from "./convert.mjs";
+import {
+  convertInchesToMeters,
+  convertInchesToCentimeters,
+  convertInchesToMillimeters,
+  isRunningInBrowser,
+  INCHES_TO_CENTIMETER,
+  INCHES_TO_MILLIMETER,
+  INCHES_TO_METER
+} from "./convert.mjs";
 
 
-let args = process.argv;
-
-console.log(args);
-const shouldTest = args.some((item) => item === "-t")
-
-if (shouldTest) {
-  testconvertInchesToMeters();
-  testConvertInchesToMillimeter();
-  testConvertInchesToCentimeter();
+function testIsRunningInBrowser() {
+  if (isRunningInBrowser()) {
+    console.log("Is running in browser test true");
+  } else {
+    console.error("is running in browser test false");
+  }
 }
 
+export function testing() {
+  testConvertInchesToMillimeter();
+  testConvertInchesToCentimeter();
+  testconvertInchesToMeters()
+  testIsRunningInBrowser();// hvis dokument eksisterer, alltid retunerer false
+}
 
+function testConvertInchesToMillimeter() {
+
+  if (convertInchesToMillimeters(1) === INCHES_TO_MILLIMETER) {
+    console.log("Test mm Passed");
+  } else {
+    console.error("Test mm Failed");
+  }
+}
+function testConvertInchesToCentimeter() {
+  if (convertInchesToCentimeters(1) === INCHES_TO_CENTIMETER) {
+    console.log("Test cm Passed");
+  } else {
+    console.error("Test cm Failed");
+  }
+}
 function testconvertInchesToMeters() {
-  if (convertInchesToMeters(1) === 0.0254) {
+  if (convertInchesToMeters(1) === INCHES_TO_METER) {
     console.log("Test meters Passed");
   } else {
     console.error("Test meters Failed");
   }
 }
 
-function testConvertInchesToCentimeter() {
-  if (convertInchesToCentimeters(1) === 2.54) {
-    console.log("Test cm Passed");
-  } else {
-    console.error("Test cm Failed");
-  }
-}
 
-function testConvertInchesToMillimeter() {
-
-  if (convertInchesToMillimeter(1) === 25.4) {
-    console.log("Test mm Passed");
-  } else {
-    console.error("Test mm Failed");
-  }
-}
